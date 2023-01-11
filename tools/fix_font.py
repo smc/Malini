d726408e692931d9f55fd3ab2c4be5b78066c3f8
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-01-11 14:23:46 +0530

    Simplify fix_font

--- tools/fix_font.py
+++ tools/fix_font.py
@@ -9,10 +9,8 @@ log = logging.getLogger(__name__)
 def fix_font(fontFile):
     log.debug(f"Fixing {fontFile}")
     ttFont = ttLib.TTFont(fontFile)
-    # add_dummy_dsig(ttFont)
     fix_unhinted_font(ttFont)
     fix_fs_type(ttFont)
-    remove_aat(ttFont)
     ttFont.save(fontFile)
 
 def fix_unhinted_font(ttFont: ttLib.TTFont):
@@ -34,38 +32,10 @@ def fix_fs_type(ttFont: ttLib.TTFont):
     Args:
         ttFont: a TTFont instance
     """
-    old = ttFont["OS/2"].fsType
+
     ttFont["OS/2"].fsType = 0
-    return old != 0
-
-def remove_aat(ttFont: ttLib.TTFont):
-    """Unwanted AAT tables were found in the font and should be removed .
-    Args:
-        ttFont: a TTFont instance
-    """
-    unwanted_tables = [
-        'EBSC', 'Zaph', 'acnt', 'ankr', 'bdat', 'bhed', 'bloc',
-        'bmap', 'bsln', 'fdsc', 'feat', 'fond', 'gcid', 'just',
-        'kerx', 'lcar', 'ltag', 'mort', 'morx', 'opbd', 'prop',
-        'trak', 'xref'
-    ]
-    for unwanted in unwanted_tables:
-        if unwanted in ttFont:
-            del ttFont[unwanted]
 
 
-def add_dummy_dsig(ttFont: ttLib.TTFont) -> None:
-    """Add a dummy dsig table to a font. Older versions of MS Word
-    require this table.
-    Args:
-        ttFont: a TTFont instance
-    """
-    newDSIG = ttLib.newTable("DSIG")
-    newDSIG.ulVersion = 1
-    newDSIG.usFlag = 0
-    newDSIG.usNumSigs = 0
-    newDSIG.signatureRecords = []
-    ttFont.tables["DSIG"] = newDSIG
 
 if __name__ == "__main__":
     for arg in sys.argv[1:]:
