cf6835260881d1c0f8688921cc3bd68310025fab
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-07-30 11:34:57 +0530

    Fine tuning

--- specimen/js/main.js
+++ specimen/js/main.js
@@ -1,12 +1,10 @@
 function addFooters(){
    document.querySelectorAll('.page footer').forEach((footer, key) => {
     footer.innerHTML=''
-    if (key == 0) {
-        return;
-    }
+
     const pageNumber = document.createElement('span')
     pageNumber.classList.add('footer-pageno')
-    pageNumber.textContent = key;
+    pageNumber.textContent = key+1;
     footer.append(pageNumber)
 
     const typeName = document.createElement('span')
