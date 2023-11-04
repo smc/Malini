fbf9944792d6d566b9cc3d923cfdc1a186575932
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2023-11-04 16:23:24 +0530

    major: specimen redesign
    Work in progress

--- specimen/js/main.js
+++ specimen/js/main.js
@@ -1,23 +1,75 @@
-function addFooters(){
-   document.querySelectorAll('.page footer').forEach((footer, key) => {
-    footer.innerHTML=''
-
-    const pageNumber = document.createElement('span')
-    pageNumber.classList.add('footer-pageno')
-    pageNumber.textContent = key+1;
-    footer.append(pageNumber)
-
-    const typeName = document.createElement('span')
-    typeName.classList.add('footer-typename')
-    typeName.textContent = 'Malini'
-    footer.append(typeName)
-    const siteName = document.createElement('span')
-    siteName.textContent = 'smc.org.in/fonts/malini'
-    siteName.classList.add('footer-site')
-    footer.append(siteName)
-});
+/** Dark, Light modes */
+const storageKey = 'theme-preference'
+let theme = 'dark';
+let root = document.documentElement;
+const setTheme = theme => {
+  root.setAttribute('color-scheme', theme)
+  localStorage.setItem(storageKey, theme)
 }
 
-window.onload = function(){
-    addFooters()
+const init = () => {
+  // now this script can find and listen for clicks on the control
+  document
+    .querySelector('#theme-toggle').addEventListener('click', () => {
+      // flip current value
+      theme = theme === 'light'
+        ? 'dark'
+        : 'light'
+
+      setTheme(theme)
+    })
+
+  document.querySelectorAll("[data-id='font-size']").forEach((element) => {
+    element.addEventListener('input', function () {
+      const fontSize = element.value;
+      if (element.type == 'range') {
+        document.querySelector('#playground-font-size').value = fontSize;
+      } else {
+        document.querySelector('input[type="range", data-id="font-size"]').value = fontSize;
+      }
+      root.style.setProperty('--playground-font-size', `${fontSize}pt`);
+    });
+  });
+
+
+  document.querySelectorAll("[data-id='font-weight']").forEach((element) => {
+    element.addEventListener('input', function () {
+      const fontWeight = element.value;
+      if (element.type == 'range') {
+        document.querySelector('#playground-font-weight').value = fontWeight;
+      } else {
+        document.querySelector('input[type="range", data-id="font-weight"]').value = fontWeight;
+      }
+      root.style.setProperty('--playground-font-weight', fontWeight);
+    });
+  });
+
+  document.querySelectorAll("[data-id='font-width']").forEach((element) => {
+    element.addEventListener('input', function () {
+      const fontwidth = element.value;
+      if (element.type == 'range') {
+        document.querySelector('#playground-font-width').value = fontwidth;
+      } else {
+        document.querySelector('input[type="range", data-id="font-width"]').value = fontwidth;
+      }
+      root.style.setProperty('--playground-font-width', `${fontwidth}%`);
+    });
+  });
+
+
+  document.querySelectorAll("[data-id='font-slant']").forEach((element) => {
+    element.addEventListener('input', function () {
+      const fontSlant = element.value;
+      if (element.type == 'range') {
+        document.querySelector('#playground-font-slant').value = fontSlant
+      } else {
+        document.querySelector('input[type="range", data-id="font-slant"]').value = fontSlant;
+      }
+      root.style.setProperty('--playground-font-slant', `${fontSlant*-1}deg`);
+    });
+  });
+
+
 }
+
+window.onload = init;
